8295b3f8efc0a30784070b87faa4ffa40f79250b
danielkick <drk8b9@mail.missouri.edu>
2021-01-28 15:16:13 -0600

    Don't calculate ellipses if not desired.

--- R/vis_pca_3D_plotly.R
+++ R/vis_pca_3D_plotly.R
@@ -14,10 +14,10 @@
 #' @examples
 
 vis_pca_3D_plotly <- function(input.df = dplyr::filter(M, pass == T) %>% dplyr::select(-lc, -net, -pass),
-                       color.by = M[M$pass == T, "lc"],
-                       # turn.x.times = 2,
-                       use.colors = RColorBrewer::brewer.pal(8, "Set1"),
-                       ellipse.opacity = 0.05){
+                              color.by = M[M$pass == T, "lc"],
+                              # turn.x.times = 2,
+                              use.colors = RColorBrewer::brewer.pal(8, "Set1"),
+                              ellipse.opacity = 0.05){
 
   res.pca <- PCA(input.df,  graph = FALSE)
 
@@ -26,37 +26,39 @@ vis_pca_3D_plotly <- function(input.df = dplyr::filter(M, pass == T) %>% dplyr::
   indivduals$cell.num <- as.factor(as.character(color.by))
 
   pca_plt <- plot_ly(indivduals,
-          x = ~Dim.1,
-          y = ~Dim.2,
-          z = ~Dim.3,
-          color = ~cell.num,
-          colors = use.colors) %>%
+                     x = ~Dim.1,
+                     y = ~Dim.2,
+                     z = ~Dim.3,
+                     color = ~cell.num,
+                     colors = use.colors) %>%
     add_markers() %>%
     layout(scene = list(xaxis = list(title = 'Dim. 1'),
                         yaxis = list(title = 'Dim. 2'),
                         zaxis = list(title = 'Dim. 3')))
 
   # add ellipses
-  # inspired by:
-  # https://stackoverflow.com/questions/50412858/plotting-ellipse3d-in-r-plotly-with-surface-ellipse
-  ellipse_list <- map(unique(indivduals$cell.num), function(ellipse.group){
-    rgl::ellipse3d(cov(
-      indivduals[
-        indivduals$cell.num == ellipse.group,
-        c("Dim.1", "Dim.2", "Dim.3")]
-    ))
-  })
+  if (ellipse.opacity <= 0){
+    # inspired by:
+    # https://stackoverflow.com/questions/50412858/plotting-ellipse3d-in-r-plotly-with-surface-ellipse
+    ellipse_list <- map(unique(indivduals$cell.num), function(ellipse.group){
+      rgl::ellipse3d(cov(
+        indivduals[
+          indivduals$cell.num == ellipse.group,
+          c("Dim.1", "Dim.2", "Dim.3")]
+      ))
+    })
 
-  for(i in seq_along(unique(indivduals$cell.num))){
-    # add ellipses one at a time so that we can accommodate an arbitrary number of them
-    pca_plt <- pca_plt %>%
-      add_trace(x=ellipse_list[[i]]$vb [1,],
-                y=ellipse_list[[i]]$vb [2,],
-                z=ellipse_list[[i]]$vb [3,],
-                colors = use.colors[i],
-                type='mesh3d',
-                alphahull = 0,
-                opacity = ellipse.opacity)
+    for(i in seq_along(unique(indivduals$cell.num))){
+      # add ellipses one at a time so that we can accommodate an arbitrary number of them
+      pca_plt <- pca_plt %>%
+        add_trace(x=ellipse_list[[i]]$vb [1,],
+                  y=ellipse_list[[i]]$vb [2,],
+                  z=ellipse_list[[i]]$vb [3,],
+                  colors = use.colors[i],
+                  type='mesh3d',
+                  alphahull = 0,
+                  opacity = ellipse.opacity)
+    }
   }
 
   return(pca_plt)
